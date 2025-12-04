8a690a4d624feadbf05dc9dbb0394144d15e40e9
Frank Obedi <frankmobedi@gmail.com>
2025-12-03 21:48:34 -0500

    Update Courses.jsx

--- src/Pages/Home/Courses.jsx
+++ src/Pages/Home/Courses.jsx
@@ -4,7 +4,7 @@ import { CourseList } from "./CourseList";
 export default function Courses() {
   return (
     <div className="courses-window">
-      <h3 className="window-heading">RELEVANT COURSES</h3>
+      <h3 className="window-heading">WORK EXPERIENCE</h3>
       <ul className="course-list">
         {CourseList.map((course) => {
           return <li key={course.courseName}>{course.courseName}</li>;
