d73cdd01e23c476bbacfe9f8be49088221a4908d
Frank Obedi <frankmobedi@gmail.com>
2025-12-03 21:50:37 -0500

    Update course names in CourseList component

--- src/Pages/Home/CourseList.jsx
+++ src/Pages/Home/CourseList.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export const CourseList = [
   {
-    courseName: "Data Structures",
+    courseName: "IT Consultant -- FDM GROUP -- Nov 2025 - Present",
   },
   {
     courseName: "Algorithm Design/Analysis",
   },
   {
-    courseName: "Operating Systems",
+    courseName: "Production Engineering Fellow -- MLH Felloship -- Jun 2024 - Sep 2024",
   },
   {
     courseName: "Database Management Systems",
