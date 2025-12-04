f635794e2d22cacb974c73af097ba77e54f76fac
Frank Obedi <frankmobedi@gmail.com>
2025-12-03 21:56:02 -0500

    Refactor course names in CourseList
    Updated course names in CourseList to be more concise.

--- src/Pages/Home/CourseList.jsx
+++ src/Pages/Home/CourseList.jsx
@@ -2,18 +2,15 @@ import React from "react";
 
 export const CourseList = [
   {
-    courseName: "IT Consultant -- FDM GROUP -- Nov 2025 - Present",
+    courseName: "IT Consultant [ FDM GROUP ]",
   },
   {
-    courseName: "Algorithm Design/Analysis",
+    courseName: "Production Engineering Fellow -- MLH Felloship",
   },
   {
-    courseName: "Production Engineering Fellow -- MLH Felloship -- Jun 2024 - Sep 2024",
+    courseName: "Web Development Intern [ Lakeshore Arts ]",
   },
   {
-    courseName: "Database Management Systems",
-  },
-  {
-    courseName: "Software Engineering",
+    courseName: "Web Development Intern -- North York Arts",
   },
 ];
